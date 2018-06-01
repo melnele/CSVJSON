57f0cf02ca22e06377e5b3e528bc56c183894c0a
melnele <mohamed.nele@gmail.com>
2018-06-01 11:18:26 +0200

    file upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { send } from 'q';
+import { convertInjectableProviderToFactory } from '@angular/core/src/di/injectable';
 
 @Component({
   selector: 'app-root',
@@ -10,24 +12,56 @@ export class AppComponent {
   title = 'JSON to CSV';
   json = '';
   csv = '';
+  static jsonfile = '';
 
-  constructor(private http: HttpClient) { }
+  constructor(public http: HttpClient) { }
 
+  static getfiletext(str) {
+    console.log(str);
+    this.jsonfile = str;
+  }
+  convert2() {
+    var config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    try {
+      JSON.parse(AppComponent.jsonfile);
+    } catch (e) {
+      console.log("not json");
+      return;
+    }
+    this.http.post('/api/jsontocsv/', AppComponent.jsonfile, config).subscribe(res => {
+      this.csv = res["data"];
+    }, err => {
+    });
+  }
   convert() {
     var config = {
       headers: {
         'Content-Type': 'application/json'
       }
     };
-    try{
+    try {
       JSON.parse(this.json);
-    }catch(e){
+    } catch (e) {
       console.log("not json");
-      return ;
+      return;
     }
     this.http.post('/api/jsontocsv/', this.json, config).subscribe(res => {
       this.csv = res["data"];
     }, err => {
     });
+  }
+
+  fileChange(event) {
+    let file: File = event.target.files[0];
+    var reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onloadend = function (e) {
+      AppComponent.getfiletext(reader.result);
+    }
   }
 }
